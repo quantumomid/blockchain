38276c36a1a019fa3a7cac543fb5bcc38fa34a6e
quantumomid <omid.wakili@outlook.com>
2021-12-20 22:48:04 +0000

    complete deploy of the Inbox contract using metamask address with Rinkeyby

--- SmartContract/deploy.js
+++ SmartContract/deploy.js
@@ -2,6 +2,7 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
+require('dotenv').config();
 
 const provider = new HDWalletProvider(
     process.env.ACCOUNT_WORDS,
@@ -9,3 +10,21 @@ const provider = new HDWalletProvider(
 );
 
 const web3 = new Web3(provider);
+
+// function to allow us to use the async-await syntax
+const deploy = async () => {
+    // Getting all of the accounts associated with our metamask address
+    const accounts = await web3.eth.getAccounts();
+    console.log("Deploying from account", accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["What's up mate?"] })
+        .send({ gas: "1000000", from: accounts[0] });
+    
+    console.log("Address the contract was deployed to: ", result.options.address);
+
+    //Below is to prevent a hanging deployment
+    provider.engine.stop();
+};
+
+deploy();
