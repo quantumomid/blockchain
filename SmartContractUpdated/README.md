13dbbaf601e7f23ff969589c829e29b3147f1d4f
quantumomid <omid.wakili@outlook.com>
2021-12-20 23:03:51 +0000

    add new changes to the Inbox.sol file

---
+++ SmartContractUpdated/README.md
@@ -0,0 +1,15 @@
+# Smart Contract Intro (with new Updates)
+
+- Use solidity and node.js to create a small contract, compile it with the solidity compiler and deploy to an Etherium network.
+
+## Updates:
+
+Inbox.sol:
+
+- New `constructor` keyword 
+- New `memory` keyword to specify the data location of the variables 
+- Removing the `public` keyword from the constructor
+- SPDX identifier at the top of the contract to address compilation warnings
+
+
+compile.js:
---
+++ SmartContractUpdated/test/Inbox.test.js
@@ -0,0 +1,44 @@
+// contract test code will go here
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const { interface, bytecode } = require("../compile");
+
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+beforeEach(async () => {
+    // Get list of all dummy accounts for our testing network from ganache
+    accounts = await web3.eth.getAccounts();
+
+    // Deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["Salaam there!"] })
+        .send({ from: accounts[0], gas: "1000000" })
+});
+
+describe("Inbox", () => {
+    it("It deploys a contract", () => {
+        // console.log(accounts);
+        // console.log(inbox);
+        assert.ok(inbox.options.address);
+    })
+
+    it("It has a default message", async () => {
+        // Call the .message() method on the Inbox contract to get the message
+        // use .call() because here we are NOT modifying the contract
+        const message = await inbox.methods.message().call();
+        assert.equal(message, "Salaam there!");
+    })
+
+    it("Message changes as expected", async () => {
+        const newMessage = "Wa alaikum mus Salaam!";
+        // Now we are MODIFYING the contract i.e. therefore will be sending a 
+        // transaction and hence we use the .send() method 
+        await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+        // Retireve message and check if it did indeed change
+        const message = await inbox.methods.message().call();
+        assert.equal(message, newMessage);
+    })
+})
