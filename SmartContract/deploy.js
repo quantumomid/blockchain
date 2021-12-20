551efc0580d6358754230cca3ddeeffb13903962
quantumomid <omid.wakili@outlook.com>
2021-12-20 21:42:52 +0000

    boiler plate code to deploy to Rinkeby network with Infura and our metamask address

--- SmartContract/deploy.js
+++ SmartContract/deploy.js
@@ -1,1 +1,11 @@
 // deploy code will go here
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const { interface, bytecode } = require("./compile");
+
+const provider = new HDWalletProvider(
+    process.env.ACCOUNT_WORDS,
+    process.env.NETWORK_LINK,
+);
+
+const web3 = new Web3(provider);
