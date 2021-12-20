6afedc08374289db8b330fa2686c1b0479660f75
quantumomid <omid.wakili@outlook.com>
2021-12-20 19:55:03 +0000

    add code to deploy our compiled smart contract using one of the dummy test accounts from ganache

--- SmartContract/test/Inbox.test.js
+++ SmartContract/test/Inbox.test.js
@@ -2,20 +2,26 @@
 const assert = require("assert");
 const ganache = require("ganache-cli");
 const Web3 = require("web3");
+const { interface, bytecode } = require("../compile");
 
 const web3 = new Web3(ganache.provider());
 
-beforeEach(() => {
+let accounts;
+let inbox;
+beforeEach(async () => {
     // Get list of all dummy accounts for our testing network from ganache
-    web3.eth.getAccounts()
-        .then(accounts => {
-            console.log(accounts);
-        });
+    accounts = await web3.eth.getAccounts();
 
+    // Deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ["Salaam there!"] })
+        .send({ from: accounts[0], gas: "1000000" })
 });
 
 describe("Inbox", () => {
     it("It deploys a contract", () => {
-        
+        console.log(accounts);
+        console.log(inbox);
+
     })
 })
