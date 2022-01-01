e8b8c1cf8dafd0c05363891c5a38fc7aad265fe7
quantumomid <omid.wakili@outlook.com>
2022-01-01 20:23:08 +0000

    use get server side prps to get request infos and pass as props into request page of each campaign

--- CampaignsProject/pages/campaigns/[campaignAddress]/requests/index.js
+++ CampaignsProject/pages/campaigns/[campaignAddress]/requests/index.js
@@ -1,10 +1,66 @@
 import { useRouter } from "next/router";
 import { Button } from "semantic-ui-react";
+import CampaignContract from "../../../../ethereum/campaign";
+import factory from "../../../../ethereum/factory";
 
-const CampaignRequests = () => {
-    const router = useRouter();
-    const address = router.query.campaignAddress;
-    console.log({ address });
+// export const getStaticPaths = async () => {
+
+//     const deployedCampaigns = await factory.methods.getDeployedCampaigns().call();
+//     console.log({deployedCampaigns});
+
+//     const campaignPaths = deployedCampaigns.map(campaignAddress => ({ params: { campaignAddress } }))
+
+//     return {
+//         paths: campaignPaths,
+//         fallback: true,
+//     }
+// }
+
+// export const getStaticProps = async ({ params }) => {
+//     console.log({params});
+//     // const deployedCampaign = CampaignContract(params.campaignAddress);
+//     // console.log(deployedCampaign);
+//     // const summary = await deployedCampaign.methods.getSummary().call();
+//     // console.log(summary);
+
+//     return {
+//         props: {
+
+//         },
+//         revalidate: 1,
+//       };
+// }
+
+export const getServerSideProps = async (context) => {
+    // console.log({context});
+    const address = context.query.campaignAddress;
+    // console.log({address});
+    const campaign = CampaignContract(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    // console.log({requestCount});
+
+    const requests = await Promise.all(
+        Array(parseInt(requestCount)).fill().map((element, index) => {
+            return campaign.methods.requests(index).call();
+        })
+    );
+    console.log({requests});
+
+    return {
+        props: {
+            address,
+            requests: JSON.parse(JSON.stringify(requests)) ,
+            requestCount
+        }
+      };
+}
+
+const CampaignRequests = ({ address, requests, requestCount }) => {
+    // const router = useRouter();
+    // const address = router.query.campaignAddress;
+    console.log({ address, requests, requestCount });
+    // const campaign = CampaignContract(address);
+
 
     return (
         <>
